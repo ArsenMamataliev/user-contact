ea93a9c948ffe88a199ee9e9dbe31410c8de807c
arsenmamataliev <a.mamataliev93@gmail.com>
2021-10-19 14:28:42 +0600

    homepage changes in package json

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contacts from "./pages/Contacts";
 function App() {
   return (
      <div className="App">
-      <Router basename="/auth" >  
+      <Router >  
         <Switch>
           <Route exact path="/auth" component ={Auth} />
           <Route  path="/contacts" component ={Contacts} />
